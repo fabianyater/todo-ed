7d821c4bcabe56f535800aaf9e0ebcd84961753b
Fabian Yate <fabianyate31@gmail.com>
2021-04-10 05:26:02 -0500

    feature(swagger): Adden swagger to document the api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ToDo API')
+    .setDescription('Descripción api ToDo')
+    .setVersion('1.0')
+    .addTag('ToDo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
